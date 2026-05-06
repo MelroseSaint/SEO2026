22557cc59c1a227bf8a7cb30ac2b3480328c41c3
[dyad] <git@dyad.sh>
2026-05-06 18:40:49 -0400

    [dyad] Removed all Dyad references and updated favicon to the correct site icon. - wrote 3 file(s)

--- src/components/made-with-dyad.tsx
+++ src/components/made-with-dyad.tsx
@@ -1,14 +1,1 @@
-export const MadeWithDyad = () => {
-  return (
-    <div className="p-4 text-center">
-      <a
-        href="https://www.dyad.sh/"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="text-sm text-gray-300 hover:text-white"
-      >
-        Made with Dyad
-      </a>
-    </div>
-  );
-};
+// Remove the MadeWithDyad component entirely
