4702d3ebd7ee4a488463163026abadeb32c19324
[dyad] <git@dyad.sh>
2026-05-06 22:42:07 -0400

    [dyad] Fixing the React Router context error by wrapping the app in BrowserRouter and restoring missing routes. - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,28 @@
 "use client";
 
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from 'next-themes';
 import { PlanProvider } from './context/PlanContext';
 import Index from './pages/Index';
+import Docs from './pages/Docs';
+import NotFound from './pages/NotFound';
 import { Toaster } from 'sonner';
 
 function App() {
   return (
-    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
-      <PlanProvider>
-        <Routes>
-          <Route path="/" element={<Index />} />
-        </Routes>
-        <Toaster position="top-center" richColors />
-      </PlanProvider>
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+        <PlanProvider>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/docs" element={<Docs />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+          <Toaster position="top-center" richColors />
+        </PlanProvider>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
